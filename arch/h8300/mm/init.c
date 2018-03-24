18aad1248fba22b706d02ded73d2438a51504063
Shea Levy <shea@shealevy.com>
2018-03-24 11:36:55 -0400

    h8300: Use INITRAMFS_GENERIC_UNLOAD
    Signed-off-by: Shea Levy <shea@shealevy.com>

--- arch/h8300/mm/init.c
+++ arch/h8300/mm/init.c
@@ -102,13 +102,6 @@ void __init mem_init(void)
 }
 
 
-#ifdef CONFIG_BLK_DEV_INITRD
-void free_initrd_mem(unsigned long start, unsigned long end)
-{
-	free_reserved_area((void *)start, (void *)end, -1, "initrd");
-}
-#endif
-
 void
 free_initmem(void)
 {
